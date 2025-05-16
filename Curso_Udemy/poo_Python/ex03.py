0d898e3123c132f04e85c1bdaaf1e3a6729d4cc6
AslefiNunes <116587731+AslefiNunes@users.noreply.github.com>
2025-05-16 16:31:50 -0300

    Exercicio da calculadora da Udemy

--- Curso_Udemy/poo_Python/ex03.py
+++ Curso_Udemy/poo_Python/ex03.py
@@ -9,7 +9,7 @@ print('*')'''
 # Crie uma calculadora com While - pergunto ao usuario 2 valores primeir é denominador peça tbm a operação que vai ser +, -, *, /
 
 n1 = 1
-while True:
+while True: 
         try:
             n2 = int(input('Digite qual a tabuda você que ver: '))
             operador = int(input('Escolha uma das opeções:\n' \
@@ -18,6 +18,10 @@ while True:
             '[3] - Multiplicação\n' \
             '[4] - Divisão\n ' \
             'Faça sua Escolha: '))
+            if operador == 2:
+                n1 = n2 + 1
+            if operador == 4:
+                n1 = n2
         except:
             print('VALORES INVALIDOS TENTE NOVAMENTE')
         else:
@@ -26,13 +30,19 @@ while True:
             else:
                 break
 
-while n1 <= 10:
-    if operador == 1:
-        print(f'{n1} + {n2} = {n1+n2}')
-    elif operador == 2:
-        print(f'{n1} - {n2} = {n1-n2}')
-    elif operador == 3:
-        print(f'{n1} x {n2} = {n1*n2}')
-    else:
-        print(f'{n1} / {n2} = {n1/n2}')
-    n1 += 1
+if operador == 1 or operador == 3:
+    while n1 <= 10:
+        if operador == 1:
+            print(f'{n1} + {n2} = {n1+n2}')
+        elif operador == 3:
+            print(f'{n1} x {n2} = {n1*n2}')
+elif operador == 2: 
+    while (n1 - n2) != 11:
+        if operador == 2 :
+            print(f'{n1} - {n2} = {n1-n2}')
+            n1 += 1
+else:
+    while (n1 / n2 ) != 11:
+        if operador == 4:
+            print(f'{n1} / {n2} = {n1/n2:.0f}')
+            n1 += n2
